0ab8a2f00d5ee20239dc7fd6c0c363928ce78e33
Leonardo Hentschke <leo.hentschke@hotmail.com>
2025-10-07 22:59:21 -0300

    wip

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -10,7 +10,7 @@ const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME || 'user',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_DATABASE || 'recolheja_db',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV === 'development',
   logging: process.env.NODE_ENV === 'development',
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
