050367ae5ae7d168373e35eb9c6af095d153bfcb
Leonardo Hentschke <leo.hentschke@hotmail.com>
2025-10-07 21:16:28 -0300

    init

--- mobile/assets/icons.tsx
+++
@@ -1,9 +0,0 @@
-import { Feather } from '@expo/vector-icons'
-import type { ComponentProps } from 'react'
-
-type FeatherIconProps = Omit<ComponentProps<typeof Feather>, 'name' | 'size'>
-
-export const icons = {
-  'map/index': (props: FeatherIconProps) => <Feather {...props} name='map' size={26} />,
-  'events/index': (props: FeatherIconProps) => <Feather {...props} name='calendar' size={26} />,
-}
